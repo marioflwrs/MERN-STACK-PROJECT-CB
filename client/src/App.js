8bd448825f2e43b9c0212f674c30d19008319c54
MarioB <wawamario@users.noreply.github.com>
2020-01-20 14:05:08 -0500

    added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link><h1>Home</h1></Link>
+        <Link><h1>About</h1></Link>
+        <Link><h1>Projects</h1></Link>
+        <Link><h1>Fun</h1></Link>
+      </div>
+    </Router>
   );
 }
 
