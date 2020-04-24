1f4379671faf7fe97b76d8984568e173705ea05e
wawamario <mariomuloz@gmail.com>
2020-04-24 00:20:04 -0400

    restart

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -5,20 +5,9 @@ const Home = () => {
     return (
         <div>
             <div className="news-container">
-                <div className="news-card">
-                    <h1>Title</h1>
-                    <img src="https://www.fillmurray.com/300/300" alt="news cover" />
-                    <p>Cillum consequat aliqua exceptexceptexcepteur est.</p>
-                </div>
-                <div className="news-card">
-                    <h1>Title</h1> 
-                    <img src="https://www.fillmurray.com/300/300" alt="news cover" />
-                    <p>Ut voluptate fugiat eu amet duis consectetur occaecat mollit.</p>
-                </div>
-                <div className="news-card">
-                    <h1>Title</h1>
-                    <img src="https://www.fillmurray.com/300/300" alt="news cover" />
-                    <p>Ullamco sunt ad esse aliquip dolor exercitation exercitation sit.</p>
+                <div className="article-container">
+                    <h1>Breaking vs Quarantine</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum arcu vitae elementum curabitur vitae nunc sed velit. Morbi blandit cursus risus at ultrices. Amet luctus venenatis lectus magna fringilla. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Interdum velit euismod in pellentesque. Commodo elit at imperdiet dui accumsan sit. Fringilla est ullamcorper eget nulla facilisi etiam. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Eu facilisis sed odio morbi quis commodo odio. Fermentum odio eu feugiat pretium.</p>
                 </div>
             </div>
         </div>
