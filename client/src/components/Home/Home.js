b51f6f38790a4cb65221540e0c8f44e0c039cf3f
wawamario <mariomuloz@gmail.com>
2020-04-28 18:08:14 -0400

    added grid system

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,10 +4,20 @@ import './Home.css';
 const Home = () => {
     return (
         <div>
-            <div className="news-container">
+            <div className="front-page-container">
                 <div className="article-container">
-                    <h1>Breaking vs Quarantine</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum arcu vitae elementum curabitur vitae nunc sed velit. Morbi blandit cursus risus at ultrices. Amet luctus venenatis lectus magna fringilla. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Interdum velit euismod in pellentesque. Commodo elit at imperdiet dui accumsan sit. Fringilla est ullamcorper eget nulla facilisi etiam. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Eu facilisis sed odio morbi quis commodo odio. Fermentum odio eu feugiat pretium.</p>
+                    <div className="article-solo-container">
+                        <h1>Article Title 1</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac turpis egestas. Augue interdum velit euismod in pellentesque massa. Morbi blandit cursus risus at ultrices mi tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Enim eu turpis egestas pretium aenean. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu. Id neque aliquam vestibulum morbi blandit cursus risus at. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Et ligula ullamcorper malesuada proin. Lacus viverra vitae congue eu consequat ac felis donec. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Odio euismod lacinia at quis. Et malesuada fames ac turpis egestas. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Semper risus in hendrerit gravida rutrum quisque.</p>
+                    </div>
+                    <div className="article-day-container">
+                        <h1>Article Title 2</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac turpis egestas. Augue interdum velit euismod in pellentesque massa. Morbi blandit cursus risus at ultrices mi tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Enim eu turpis egestas pretium aenean. Dignissim cras tincidunt lobortis feugiat vivamusequat ac felis donec. Facilisi morbi tempus iaculis urna id volutpat lacus laoreet non. Odio euismod lacinia at quis. Et malesuada fames ac turpis egestas. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Semper risus in hendrerit gravida rutrum quisque.</p>
+                    </div>
+                    <div className="article-day-container">
+                        <h1>Article Title 3</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac turpis egestas. Augue interdum velit euismod in pellentesque massa. Morbi blandit cursus risus at ultrices mi tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Enim eu turpis egestas pretium aenean. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu. Id neque aliquam vestibulum morbi blandit cursus risus at. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Et ligula ullamcorper malesuada proin. Lacus viverra vitae congue eu consequat ac felis donec. Et malesuada fames ac turpis egestas. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Semper risus in hendrerit gravida rutrum quisque.</p>
+                    </div>
                 </div>
             </div>
         </div>
