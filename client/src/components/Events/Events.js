703398936cb77105da0752d1efbb2397bb605420
wawamario <mariomuloz@gmail.com>
2020-04-30 02:08:07 -0400

    added styling

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Events.css';
 
 const Jams = () => {
     return (
         <div>
-          
-          
+          <div className="event-cards-container">
+              <div className="event-card-create event-card"><button>Create Event</button></div>
+              <div className="event-card-view event-card"><button>View Events</button></div>
+          </div>          
         </div>
     );
 }
--- client/src/components/Forum/Forum.js
+++ client/src/components/Forum/Forum.js
@@ -5,6 +5,15 @@ import './Forum.css';
 const Forum = () => {
     return (
         <div>
+            <div className="forum-container">
+                <div className="thread">
+                    <h1>Thread 1</h1>
+                    <h3>Author</h3>
+                    <p>Description</p>
+                    <p>Date posted</p>
+                    <p>Link to open thread</p>
+                </div>
+            </div>
         </div>
     );
 }
