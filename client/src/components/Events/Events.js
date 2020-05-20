7ac26e7e96fa84a3503cc7f0880ba7cf350b6f58
wawamario <mariomuloz@gmail.com>
2020-05-20 14:47:17 -0400

    added event handler along with pushing new data onto an already created hard coded list of sample data. This is just practice leading up to something bigger!

--- client/src/components/Events/Events.js
+++ client/src/components/Events/Events.js
@@ -7,15 +7,49 @@ const Events = () => {
         {id: 'el2', text: 'event 2'},
         {id: 'el3', text: 'event 3'},
     ]
+
+    const addNewEventHandler = (newEvent) => {
+        eventList.push(newEvent);
+        console.log(eventList);
+    };
+
+
     return (
         <div>
           <div className="event-cards-container">
+            <Createevent onCreateEvent={addNewEventHandler} />
             <Viewevents elist={eventList} />
-            <Createevent />
           </div>
         </div>
     );
 }
+
+
+const Createevent = props => {
+    const addEventHandler = event => {
+        event.preventDefault();
+
+        const newEvent = {
+            id: Math.random().toString(),
+            text: 'My new event!'
+        };
+
+        props.onCreateEvent(newEvent);
+    };
+
+    return (
+        <div>
+            <div className="event-container">
+                <h1>Create Event</h1>
+                <form className="new-event" onSubmit={addEventHandler}>
+                    <input type="text" />
+                    <button type="submit">Add Event</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
 
 const Viewevents = props => {
     console.log(props.elist);
@@ -27,16 +61,6 @@ const Viewevents = props => {
                     return <li key={list.id}>{list.text}</li>
                 })
             }</ul>
-        </div>
-    );
-}
-
-const Createevent = () => {
-    return (
-        <div>
-            <div className="event-container">
-                <h1>Create Event</h1>
-            </div>
         </div>
     );
 }
