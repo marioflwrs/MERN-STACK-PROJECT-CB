1f8bbf9cb2948021c3f7987935b2126cd27206d0
wawamario <mariomuloz@gmail.com>
2020-05-25 01:21:29 -0400

    users components has been added

---
+++ client/src/components/user/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Users.css';
+
+const Users = () => {
+    return (
+        <div>
+            <h1>Users for Cypherbreak</h1>
+        </div>
+    );
+}
+
+export default Users;   
