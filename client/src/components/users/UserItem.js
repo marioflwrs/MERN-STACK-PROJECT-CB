27357a6d7447976aba01efd61f7c11a01553f008
wawamario <mariomuloz@gmail.com>
2020-06-01 16:41:10 -0400

    fix

--- client/src/components/users/UserItem.js
+++ client/src/components/users/UserItem.js
@@ -15,7 +15,7 @@ const UserItem = props => {
                 </div>
                 <div className="user-item__info">
                     <h2>{props.name}</h2>
-                    <h3>{props.location}</h3>
+                    <h3>{props.location}</h3> 
                     {/*<h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>*/}
                 </div>
             </Link>
